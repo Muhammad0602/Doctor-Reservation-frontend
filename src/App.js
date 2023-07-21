156c1fd273331fbdf2634b8cab875469311b2e75
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-22 01:52:01 +0500

    Add the home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getDoctors } from '../redux/doctors/doctorsSlice';
+
+function Home() {
+  const { doctors, isLoading, error } = useSelector((store) => store.doctors);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getDoctors());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <div className="spinner" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="error-container">
+        <h2>Oopps somethings went wrong.PLease try again!</h2>
+        <p>{error}</p>
+      </div>
+    );
+  }
+  return (
+    <h1>Hello I am the home page</h1>
+  )
+}
