1ab92a863bd816cbf2abbbfa398645b31381ad6a
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-07-22 22:57:21 +0500

    Apply Active Class to all Nav links

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import {
   FaTwitter, FaFacebookF, FaVine, FaPinterestP,
@@ -6,51 +6,54 @@ import {
 import { TiSocialGooglePlus } from 'react-icons/ti';
 import './sidebar.css';
 
-const Sidebar = () => (
-  <div className="sidebar_container flex">
-    <div className="sidebar_container-uperhead flex">
-      <div className="sidebar_container-logo">Logo</div>
-      <div className="sidebar_container-list">
-        <ul className="sidebar_list">
-          <li className="sidebar-link">
-            <Link className="sidebar-link-doctor" to="/home">
-              Doctor
-            </Link>
-          </li>
-          <li className="sidebar-link">
-            <Link className="sidebar-link-doctor" to="/reserveform">
-              Reserve
-            </Link>
-          </li>
-          <li className="sidebar-link">
-            <Link className="sidebar-link-doctor" to="/myreservations">
-              Reservations
-            </Link>
-          </li>
-          <li className="sidebar-link">
-            <Link className="sidebar-link-doctor" to="/adddoctor">
-              Add Doctor
-            </Link>
-          </li>
-          <li className="sidebar-link">
-            <Link className="sidebar-link-doctor" to="/deldoctor">
-              Del Doctor
-            </Link>
-          </li>
-        </ul>
+const Sidebar = () => {
+  const [activeNav, setActiveNav] = useState('home');
+  return (
+    <div className="sidebar_container flex">
+      <div className="sidebar_container-uperhead flex">
+        <div className="sidebar_container-logo">Logo</div>
+        <div className="sidebar_container-list">
+          <ul className="sidebar_list">
+            <li className="sidebar-link">
+              <Link onClick={() => setActiveNav('home')} className={activeNav === 'home' ? 'active' : ''} to="/home">
+                Doctor
+              </Link>
+            </li>
+            <li className="sidebar-link">
+              <Link onClick={() => setActiveNav('reserveform')} className={activeNav === 'reserveform' ? 'active' : ''} to="/reserveform">
+                Reserve
+              </Link>
+            </li>
+            <li className="sidebar-link">
+              <Link onClick={() => setActiveNav('myreservations')} className={activeNav === 'myreservations' ? 'active' : ''} to="/myreservations">
+                Reservations
+              </Link>
+            </li>
+            <li className="sidebar-link">
+              <Link onClick={() => setActiveNav('adddoctor')} className={activeNav === 'adddoctor' ? 'active' : ''} to="/adddoctor">
+                Add Doctor
+              </Link>
+            </li>
+            <li className="sidebar-link">
+              <Link onClick={() => setActiveNav('deldoctor')} className={activeNav === 'deldoctor' ? 'active' : ''} to="/deldoctor">
+                Del Doctor
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div className="sidbar_footer flex">
-      <div className="sidebar_socials flex">
-        <FaTwitter />
-        <FaFacebookF />
-        <TiSocialGooglePlus />
-        <FaVine />
-        <FaPinterestP />
+      <div className="sidbar_footer flex">
+        <div className="sidebar_socials flex">
+          <FaTwitter />
+          <FaFacebookF />
+          <TiSocialGooglePlus />
+          <FaVine />
+          <FaPinterestP />
+        </div>
+        <small>&copy; 2023 APPOINTMENT</small>
       </div>
-      <small>&copy; 2023 APPOINTMENT</small>
+      <Outlet />
     </div>
-    <Outlet />
-  </div>
-);
+  );
+};
 export default Sidebar;
