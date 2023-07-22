675b904c7df7d794bdcadb5d047f534448c02fee
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-07-22 20:49:43 +0500

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import Login from './components/loginpage/Login';
+import AddDcotor from './components/addDoctor/AddDoctor';
+import DelDoctor from './components/DelDoctor/DelDoctor';
+import Reserv from './components/reserve/Reserv';
+import Reservation from './components/reservation/Reservation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Sidebar />
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="home" element={<Home />} />
+        <Route path="adddoctor" element={<AddDcotor />} />
+        <Route path='deldoctor' element={<DelDoctor />} />
+        <Route path='reserveform' element={<Reserv />} />
+        <Route path='myreservations' element={<Reservation />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
