4fa754c4c689d83ee8b1f21019b7518cbc6bf48e
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-25 04:10:23 +0500

    Add a link for the delete doctor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
+import DeleteDoctor from './components/DeleteDoctor';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/delete" element={<DeleteDoctor />} />
       </Routes>
     </div>
   );
