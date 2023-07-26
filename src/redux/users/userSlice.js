1fb325c0c1ed36363a5e6bf8169fcb182e136e29
iqbal elham <iqbal.elham7@gmail.com>
2023-07-26 13:32:35 +0430

    Add userslice and declare it in store.js

--- src/redux/users/userSlice.js
+++ src/redux/users/userSlice.js
@@ -1,43 +1,39 @@
+// userSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const signupUser = createAsyncThunk('users/signupUser', async (user, thunkAPI) => {
-  try {
-    const response = await axios.post('http://localhost:3000/users', user);
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue({ error: error.response.data.error });
-  }
+export const signupUser = createAsyncThunk('user/signupUser', async (user) => {
+  const response = await axios.post('http://localhost:3000/users', user);
+  return response.data;
 });
 
-export const loginUser = createAsyncThunk('users/loginUser', async (user, thunkAPI) => {
-  try {
-    const response = await axios.post('http://localhost:3000/sessions', user);
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue({ error: error.response.data.error });
-  }
+export const loginUser = createAsyncThunk('user/loginUser', async (username) => {
+  const response = await axios.get(`http://localhost:3000/users/${username}`);
+  return response.data;
 });
 
-export const userSlice = createSlice({
+const userSlice = createSlice({
   name: 'user',
-  initialState: { user: {}, error: null },
+  initialState: { user: null, status: 'idle', error: null },
   reducers: {},
-  extraReducers: {
-    [signupUser.fulfilled]: (state, action) => {
-      state.user = action.payload.user;
-      localStorage.setItem('token', action.payload.token);
-    },
-    [signupUser.rejected]: (state, action) => {
-      state.error = action.payload.error;
-    },
-    [loginUser.fulfilled]: (state, action) => {
-      state.user = action.payload.user;
-      localStorage.setItem('token', action.payload.token);
-    },
-    [loginUser.rejected]: (state, action) => {
-      state.error = action.payload.error;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signupUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload.data;
+      })
+      .addCase(signupUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload.data;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
   },
 });
 
