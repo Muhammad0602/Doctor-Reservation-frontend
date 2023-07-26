8c2e8e922fc8f8a2c83f995681c7963f5fe93ed6
iqbal elham <iqbal.elham7@gmail.com>
2023-07-26 13:33:07 +0430

    Add login and signup route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 import Login from './components/loginpage/Login';
+import Signup from './components/signupPage/Signup';
 import AddDcotor from './components/addDoctor/AddDoctor';
 import DelDoctor from './components/DelDoctor/DelDoctor';
 import Reserv from './components/reserve/Reserv';
@@ -24,8 +25,8 @@ function App() {
       <Sidebar />
       <Routes>
         {/* <Route index element={<Login />} /> */}
-        {/* <Route path="/signup" component={Signup} /> */}
-        <Route path="/login" component={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="adddoctor" element={<AddDcotor />} />
         <Route path="deldoctor" element={<DelDoctor />} />
