2ef28f76e3a00f6f3c35ac8900e04f4b42295b67
Ayomipo Majek <ayomipomajek@gmail.com>
2023-07-27 17:05:08 +0100

    my reservation css worked on

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteReserve, getReserve } from '../redux/reservations/ReservationSlice';
+import './componentsCss/myreservations.css';
 
 const MyReservation = () => {
   const dispatch = useDispatch();
@@ -30,11 +31,11 @@ const MyReservation = () => {
   };
 
   return (
-    <div>
+    <div className="container-3">
       <h1>My Reservations</h1>
-      <div>
+      <div className="filter">
         <h3>Filter by Doctor:</h3>
-        <select value={doctor} onChange={handleDoctorChange}>
+        <select value={doctor} onChange={handleDoctorChange} className="select-option2">
           <option value={1}>Doctor 1</option>
           <option value={2}>Doctor 2</option>
         </select>
@@ -42,28 +43,30 @@ const MyReservation = () => {
       <ul>
         {reservations.map((reservation) => (
           <li key={reservation.id}>
-            <p>
+            <p className="doctor2">
               Doctor:
               {reservation.doctorName}
             </p>
-            <p>
+            <p className="city2">
               City:
               {reservation.city}
             </p>
-            <p>
+            <p className="date2">
               Date:
               {reservation.date}
             </p>
-            <p>
+            <p className="time2">
               Time:
               {reservation.appointment_time}
             </p>
-            <button type="button" onClick={() => handleDelete(reservation.id)}>Delete</button>
+            <div className="btn-container">
+              <button type="button" onClick={() => handleDelete(reservation.id)} className="del">Delete</button>
+            </div>
           </li>
         ))}
       </ul>
-      {reservations.length === 0 && <p>No reservations found.</p>}
-      <div>
+      {reservations.length === 0 && <p className="no-found">No reservations found.</p>}
+      <div className="back">
         <Link to="/">Back to Home</Link>
       </div>
     </div>
