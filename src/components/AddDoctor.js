11002755e79bf5f0b6c5255bc1f2d810096c8952
iqbal elham <iqbal.elham7@gmail.com>
2023-07-31 11:41:43 +0430

    Add style for add doctor componenets

--- src/components/AddDoctor.js
+++ src/components/AddDoctor.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import DoctorForm from './addDoctorForm/DoctorForm';
+import './addDoctorForm/doctor.css';
 
 const AddDcotor = () => (
-  <div>
-    <h1>Add Doctor Page</h1>
+  <div className="add-doctor-container">
+    <h1>Add a new Doctor</h1>
     <DoctorForm />
+    <embed src="https://lottie.host/?file=e508658a-d3ad-4854-9809-830fd303ce38/tAnKJn1r42.json" className="icon" />
   </div>
 );
 
--- src/components/addDoctorForm/DoctorForm.js
+++ src/components/addDoctorForm/DoctorForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addDoctor } from '../../redux/doctors/doctorsSlice';
-import './doctor.css';
 
 const DoctorForm = () => {
   const [name, setName] = useState('');
@@ -26,14 +25,13 @@ const DoctorForm = () => {
   };
 
   return (
-    <div className="from-container">
-      <h2>Add a Doctor</h2>
+    <div className="form-container">
       {status === 'failed' && <div>{error}</div>}
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="text" value={about} onChange={(e) => setAbout(e.target.value)} />
-        <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} />
-        <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+        <input type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} placeholder="Photo (URL)" />
+        <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} placeholder="Price per hour" />
+        <textarea type="text" value={about} onChange={(e) => setAbout(e.target.value)} placeholder="About" />
         <button type="submit" disabled={status === 'loading'}>
           Add Doctor
         </button>
