573e3941758b747083c88116aee60ac5f1d35fc4
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-08-01 03:45:21 +0500

    Update App.js, make the login the default page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
 import Login from './components/loginpage/Login';
 import Signup from './components/signupPage/Signup';
-import Private from './components/Private';
 import DeleteDoctor from './components/DeleteDoctor';
 import AddDcotor from './components/AddDoctor';
 import Reserv from './components/Reserv';
+import Private from './components/Private'
 import Reservations from './components/Reservations';
 import Home from './components/Home';
 import Sidebar from './components/Sidebar';
@@ -21,20 +17,38 @@ function App() {
   if (token) {
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
   }
+
   return (
     <BrowserRouter>
-      <Sidebar />
+      <AppContent />
+    </BrowserRouter>
+  );
+}
+
+function AppContent() {
+  // Get the current location
+  const location = useLocation();
+  // Check if the current route matches the login or signup path
+  const isLoginPage = location.pathname === '/login';
+  const isSignupPage = location.pathname === '/signup';
+
+  // Render the Sidebar only if the route is not the login or signup page
+  const renderSidebar = !isLoginPage && !isSignupPage;
+
+  return (
+    <>
+      {renderSidebar && <Sidebar />}
       <Routes>
+        <Route index element={<Private><Home /></Private>} />
         <Route path="signup" element={<Signup />} />
-        {/* <Route index element={<Login />} /> */}
         <Route path="login" element={<Login />} />
-        <Route path="home" element={<Private><Home /></Private>} />
+        <Route path="home" element={<Private><Home /></Private> } />
         <Route path="adddoctor" element={<Private><AddDcotor /></Private>} />
         <Route path="delete" element={<Private><DeleteDoctor /></Private>} />
         <Route path="reserveform" element={<Private><Reserv /></Private>} />
         <Route path="myreservations" element={<Private><Reservations /></Private>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
