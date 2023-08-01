98bdcd83e36afac46418076ac88534be0ae68ecf
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-08-01 15:32:47 +0500

    Update login.js, add link to signup

--- src/components/loginpage/Login.js
+++ src/components/loginpage/Login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../redux/users/userSlice';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './login.css';
 
 const Login = () => {
