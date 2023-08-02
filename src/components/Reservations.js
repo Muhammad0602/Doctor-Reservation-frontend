cfc499930bb05de742c38a9c5251a593bd02b174
Ayomipo Majek <ayomipomajek@gmail.com>
2023-08-02 09:07:19 +0100

    excess comments cleared

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -3,17 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getReservations } from '../redux/reservations/reservationsSlice';
 import './reservation.css';
 
-// const formatAppointmentTime = (appointmentTime) => {
-//   const date = new Date(appointmentTime);
-//   const day = date.getDate();
-//   const monthNames = [
-//     'January', 'February', 'March', 'April', 'May', 'June',
-//     'July', 'August', 'September', 'October', 'November', 'December',
-//   ];
-//   const month = monthNames[date.getMonth()];
-//   const year = date.getFullYear();
-//   return `${day} ${month}, ${year}`;
-// };
 function Reservations() {
   const { reservations, isLoading, error } = useSelector((store) => store.reservations);
   const dispatch = useDispatch();
@@ -35,23 +24,17 @@ function Reservations() {
       </div>
     );
   }
-  if (error) {
-    return (
-      <div className="error-container">
-        <h2>Oopps somethings went wrong.PLease try again!</h2>
-        <p>{error}</p>
-      </div>
-    );
-  }
   return (
     <div className="reservation-page flex">
       {reservations.map((reservation) => (
         <div className="reservation-info flex" key={reservation.id}>
           <img className="photo" src={reservation.photo} alt={reservation.name} />
           <h1>{reservation.doctor_name}</h1>
-          <p>{reservation.city}</p>
-          <p>{reservation.appointment_date}</p>
-          <p>{reservation.appointment_time}</p>
+          <div className="redetails">
+            <p>{reservation.city}</p>
+            <p>{reservation.appointment_date}</p>
+            <p>{reservation.appointment_time}</p>
+          </div>
         </div>
       ))}
     </div>
